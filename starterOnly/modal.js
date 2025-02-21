f800c0a915790dea5a0c2c1ef2f19d08815195ec
Alain Daly <alain.daly.93600@gmail.com>
2025-02-21 14:13:09 +0100

    addition function surname and formatting email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,15 +29,16 @@ function editNav() {
 
 // Section Formulaire
 const form = document.querySelector("form");
-const formImput = document.querySelector('input[type="text"]');
+const formImput = document.querySelector(".formData");
 let errorMessage = "";
+
 
 // FIRSTNAME 
 const firstnameInput = document.getElementById("firstname");
 let validateFirstname = "";
 
 firstnameInput.addEventListener('input', () => {
-  validateFirstname = firstnameInput.value.trim(); // .trim() suppression des espaces inutiles
+  validateFirstname = firstnameInput.value.trim(); // .trim() suppression des espaces inutiles et ne les prend pas en compte.
 
   if (validateFirstname.length < 2) {
     errorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
@@ -49,6 +50,55 @@ firstnameInput.addEventListener('input', () => {
   }
 
   console.log(validateFirstname);
+});
+
+
+// SURNAME 
+const surnameInput = document.getElementById("surname");
+let validateSurname = "";
+
+surnameInput.addEventListener("input", () => {
+  validateSurname = surnameInput.value.trim();
+
+  if (validateSurname.length < 2){
+    errorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    surnameInput.style.border = "2px solid red";
+    surnameInput.textContent = errorMessage;
+  } else {
+    errorMessage = "";
+    surnameInput.style.border = "none";
+  }
+
+  console.log(validateSurname);
+});
+
+
+// EMAIL 
+const emailInput = document.getElementById("email");
+let validateEmail = "";
+
+emailInput.addEventListener("input", () => {
+  validateEmail = emailInput.value.trim();
+
+  if ( validateEmail === ""){
+    errorMessage = "Le champs email ne peut pas être vide.";
+    emailInput.style.border = "2px solid red";
+  } else if (!validateEmail.includes("@")) {
+    errorMessage = "Veuillez inclure '@' dans l'adresse e-mail il manque un symbole '@'";
+    emailInput.style.border = "2px solid red";
+  } else if (!validateEmail.split("@")[1]) {
+    errorMessage = "Veuillez  saisir la partie manquante après le symbole '@'. L'adresse est incomplète";
+    emailInput.style.border = "2px solid red";
+  } else if (!validateEmail.includes(".")) {
+    errorMessage = "Le domaine de l'email doit contenir un '.'";
+    emailInput.style.border = "2px solid red";
+  } else {
+    errorMessage = "";
+    emailInput.style.border = "none";   
+  }
+
+  console.log(validateEmail);
+  
 });
 
 
