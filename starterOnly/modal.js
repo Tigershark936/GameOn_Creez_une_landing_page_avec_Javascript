e75c8201e143f16a49a0e5191ffde6d008de41d7
Alain Daly <alain.daly.93600@gmail.com>
2025-02-21 11:48:56 +0100

    Function firstname active and closeModalButton closed in click

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,7 +2,6 @@
 const closeModalButton = document.querySelector(".close");
 const modalBackground = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formImput = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -12,6 +11,11 @@ function launchModal() {
   modalBackground.style.display = "block";
 }
 
+// Close modal 
+closeModalButton.addEventListener('click', () => {
+  modalBackground.style.display = "none";
+});
+
 function editNav() {
   var topNavbar = document.getElementById("myTopnav");
   if (topNavbar.className === "topnav") {
@@ -20,3 +24,45 @@ function editNav() {
     topNavbar.className = "topnav";
   }
 }
+
+//--------------------------------------------------------------
+
+// Section Formulaire
+const form = document.querySelector("form");
+const formImput = document.querySelector('input[type="text"]');
+let errorMessage = "";
+
+// FIRSTNAME 
+const firstnameInput = document.getElementById("firstname");
+let validateFirstname = "";
+
+firstnameInput.addEventListener('input', () => {
+  validateFirstname = firstnameInput.value.trim(); // .trim() suppression des espaces inutiles
+
+  if (validateFirstname.length < 2) {
+    errorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    firstnameInput.style.border = "2px solid red";
+    firstnameInput.textContent = errorMessage;
+  } else {
+    errorMessage = ""; // Pas d'erreur de nombres de caractères
+    firstnameInput.style.border = "none";
+  }
+
+  console.log(validateFirstname);
+});
+
+
+
+// VALIDATION OF FORM
+form.addEventListener( "submit", (e) => {
+  e.preventDefault();
+
+  if (checkbox1.checked) {
+    alert ('Merci pour votre inscription');
+    modalBackground.style.display = "none";
+  } else {
+    alert ("Veuillez accepter les CVG");
+  }
+});
+
+
